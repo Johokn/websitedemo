66386ebb0d21cbd808e28152c1c19a347547d17b
Johokn <78658470+Johokn@users.noreply.github.com>
2026-05-04 22:10:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-// Function to toggle visibility of sections
 function toggleSection(header) {
     const content = header.nextElementSibling;
-    const arrow = header.textContent.includes('▼') ? '▲' : '▼';
-    header.innerHTML = header.innerHTML.replace(/▲|▼/, arrow);
-    
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-    } else {
-        content.style.display = "none";
-    }
+    const isOpen = content.style.display === "block";
+
+    content.style.display = isOpen ? "none" : "block";
+    header.classList.toggle("open");
 }
